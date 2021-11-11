c2f9a93c1f43e69c4a351b440a8b22e9b7a644dc
Samyuktha7499 <samyukthakondaparthi@gmail.com>
2021-11-10 23:10:11 -0600

    this is final commit

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -21,7 +21,26 @@ exports.shoes_detail = function(req, res) {
 // Handle Shoes create on POST. 
 exports.shoes_create_post = function(req, res) { 
     res.send('NOT IMPLEMENTED: Shoes create POST'); 
+}; exports.shoes_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Shoes(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.type = req.body.type; 
+    document.brand = req.body.brand; 
+    document.size = req.body.size; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
+ 
  
 // Handle Shoes delete form on DELETE. 
 exports.shoes_delete = function(req, res) { 
@@ -32,3 +51,15 @@ exports.shoes_delete = function(req, res) {
 exports.shoes_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: Shoes update PUT' + req.params.id); 
 };
+// VIEWS 
+// Handle a show all view 
+exports.shoes_view_all_Page = async function(req, res) { 
+    try{ 
+        theShoes = await Shoes.find(); 
+        res.render('shoes', { title: 'Shoes Search Results', results: theShoes }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
