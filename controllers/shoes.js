90c499c22f1c48fc8aef02fab558d3a7a6d53c54
Samyuktha7499 <samyukthakondaparthi@gmail.com>
2021-11-17 21:13:12 -0600

    this is 4th commit

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -114,3 +114,17 @@ exports.shoes_delete = async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+
+// Handle building the view for creating a costume. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.shoes_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('shoescreate', { title: 'Shoes Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
