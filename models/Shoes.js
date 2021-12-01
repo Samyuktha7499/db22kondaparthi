ec7db333374f629f5fe9b27115367240cfd74e69
Samyuktha7499 <samyukthakondaparthi@gmail.com>
2021-11-30 23:52:53 -0600

    this is first commit

--- models/Shoes.js
+++ models/Shoes.js
@@ -1,8 +1,12 @@
-const mongoose = require("mongoose") 
-const shoesSchema = mongoose.Schema({ 
- type: String, 
- brand: String, 
- size: Number 
-}) 
+// const mongoose = require("mongoose") 
+// const shoesSchema = mongoose.Schema({ 
+//  type: String, 
+//  brand: String, 
+//  size: {type:Number,min:15,max:100} 
+// }) 
  
-module.exports = mongoose.model("Shoes", shoesSchema)
+// module.exports = mongoose.model("Shoes", shoesSchema)
+
+const mongoose = require("mongoose")
+const ShoesSchema = mongoose.Schema({type: String,brand: String,size: {type:Number,min:5,max:100}})
+module.exports = mongoose.model("Shoes",ShoesSchema)
--- routes/shoes.js
+++ routes/shoes.js
@@ -1,6 +1,13 @@
 var express = require('express');
 const shoes_controlers= require('../controllers/shoes');
 var router = express.Router();
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
 /* GET home page. */
 router.get('/', shoes_controlers.shoes_view_all_Page ); 
@@ -8,11 +15,11 @@ router.get('/', shoes_controlers.shoes_view_all_Page );
 router.get('/detail', shoes_controlers.shoes_view_one_Page);
 
 /* GET create shoes page */ 
-router.get('/create', shoes_controlers.shoes_create_Page);
+router.get('/create', secured,shoes_controlers.shoes_create_Page);
 
 /* GET create update page */ 
-router.get('/update', shoes_controlers.shoes_update_Page); 
+router.get('/update', secured,shoes_controlers.shoes_update_Page); 
 /* GET create shoes page */ 
-router.get('/delete', shoes_controlers.shoes_delete_Page); 
+router.get('/delete', secured,shoes_controlers.shoes_delete_Page); 
 
 module.exports = router;
